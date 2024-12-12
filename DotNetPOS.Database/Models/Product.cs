1fd77f7ce2f00d764f13fca0cfa7adbae09209de
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-12 22:28:53 +0630

    feat : database first

---
+++ DotNetPOS.Database/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNetPOS.Database.Models;
+
+public partial class Product
+{
+    public int ProductId { get; set; }
+
+    public string ProductCode { get; set; } = null!;
+
+    public string ProductName { get; set; } = null!;
+
+    public decimal Price { get; set; }
+
+    public string? ProductCategoryCode { get; set; }
+
+    public virtual ProductCategory? ProductCategoryCodeNavigation { get; set; }
+
+    public virtual ICollection<SaleDetail> SaleDetails { get; set; } = new List<SaleDetail>();
+}
---
+++ DotNetPOS.Database/Models/ProductCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNetPOS.Database.Models;
+
+public partial class ProductCategory
+{
+    public int ProductCategoryId { get; set; }
+
+    public string ProductCategoryCode { get; set; } = null!;
+
+    public string ProductCategoryName { get; set; } = null!;
+
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+}
