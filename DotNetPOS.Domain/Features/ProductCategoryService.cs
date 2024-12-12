f20726cf4193e6f508e0c0d998f72b6993b8afa8
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-13 01:01:50 +0630

    feat : add Product Category Service for CURD

--- DotNetPOS.Domain/Features/ProductCategoryService.cs
+++ DotNetPOS.Domain/Features/ProductCategoryService.cs
@@ -3,10 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DotNetPOS.Database.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DotNetPOS.Domain.Features
 {
     public class ProductCategoryService
     {
+        private readonly AppDbContext _db = new AppDbContext();
+        public List<TblProductCategory> GetProductCategories()
+        {
+            var lst = _db.TblProductCategories.ToList();
+            return lst;
+        }
+        public TblProductCategory GetProductCategoryByCode(string code)
+        {
+            var item = _db.TblProductCategories
+                .AsNoTracking()
+                .FirstOrDefault(x => x.ProductCategoryCode == code)!;
+            if (item is null)
+                return null!;
+            return item;
+        }
+        public int PostProductCategory(string categoryName)
+        {
+            var item = new TblProductCategory
+            {
+                ProductCategoryCode = "PC-" + Ulid.NewUlid().ToString(),
+                Name = categoryName
+            };
+            _db.Add(item);
+            var result = _db.SaveChanges();
+            return result;
+        }
+
+        public int UpdateProductCategory(string categoryCode , string categoryName)
+        {
+            var item = _db.TblProductCategories
+                .AsNoTracking()
+                .FirstOrDefault(x => x.ProductCategoryCode == categoryCode);
+            if(item is null)
+                return 0;
+            if(!string.IsNullOrEmpty(categoryName))
+                item.Name = categoryName;
+            _db.Entry(item).State = EntityState.Modified;
+            var result = _db.SaveChanges();
+            return result;
+        }
+        public int DeleteProductCategory(string categoryCode)
+        {
+            var item = _db.TblProductCategories
+                .AsNoTracking()
+                .FirstOrDefault(x =>x.ProductCategoryCode == categoryCode);
+            if (item is null)
+                return 0;
+            _db.Entry(item).State = EntityState.Deleted;
+           var result = _db.SaveChanges();
+            return result;
+        }
     }
 }
