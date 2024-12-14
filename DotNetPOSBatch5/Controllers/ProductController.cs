254d62bc798681df2f46bd5e8cc1d1ae8a1251e4
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-14 23:24:09 +0630

    feat: product get list and get by code

---
+++ DotNetPOSBatch5/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using DotNetPOS.Domain.Features;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetPOSBatch5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductService productService = new ProductService();
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var lst = productService.GetProducts();
+            if (lst is null)
+            {
+                return Ok(new { message = "Product List is Empty" });
+            }
+            return Ok(lst);
+        }
+
+        [HttpGet("{productCode}")]
+        public IActionResult GetProduct(string productCode)
+        {
+            var item = productService.GetProduct(productCode);
+            if (item is null) return NotFound(new {message = "Product Not Found"});
+            return Ok(item);
+        }
+    }
+}
