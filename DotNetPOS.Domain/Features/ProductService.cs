feca01a673515047955849bd4a808303db573322
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-16 22:37:20 +0630

    -

--- DotNetPOS.Domain/Features/ProductService.cs
+++ DotNetPOS.Domain/Features/ProductService.cs
@@ -8,61 +8,60 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.XPath;
 
-namespace DotNetPOS.Domain.Features
+namespace DotNetPOS.Domain.Features;
+
+public class ProductService
 {
-    public class ProductService
+    private readonly AppDbContext _db = new AppDbContext();
+    public List<TblProduct> GetProducts()
     {
-        private readonly AppDbContext _db = new AppDbContext();
-        public List<TblProduct> GetProducts()
-        {
-            var lst = _db.TblProducts.Where(x => x.DeleteFlag == false).ToList();
-            return lst;
-        }
+        var lst = _db.TblProducts.Where(x => x.DeleteFlag == false).ToList();
+        return lst;
+    }
 
-        public TblProduct GetProduct(string productCode)
+    public TblProduct GetProduct(string productCode)
+    {
+        var item = _db.TblProducts.FirstOrDefault(x => x.ProductCode == productCode);
+        if (item is null)
         {
-            var item = _db.TblProducts.FirstOrDefault(x => x.ProductCode == productCode);
-            if (item is null)
-            {
-                return null;
-            }
-            return item;
+            return null;
         }
+        return item;
+    }
 
-        public int ProductCreate(string productName, double price, string productCategoryCode)
+    public int ProductCreate(string productName, double price, string productCategoryCode)
+    {
+        TblProduct product = new TblProduct()
         {
-            TblProduct product = new TblProduct()
-            {
-                ProductCode = Ulid.NewUlid().ToString(),
-                Name = productName,
-                Price = price,
-                ProductCategoryCode = productCategoryCode,
-                DeleteFlag = false
-            };
-            
-             _db.TblProducts.Add(product);
-            var result = _db.SaveChanges();
-            return result;
-        }
+            ProductCode = Ulid.NewUlid().ToString(),
+            Name = productName,
+            Price = price,
+            ProductCategoryCode = productCategoryCode,
+            DeleteFlag = false
+        };
+        
+         _db.TblProducts.Add(product);
+        var result = _db.SaveChanges();
+        return result;
+    }
 
-       public int ProductUpdate(string productCode, string productName, double price, string productCategoryCode )
-        {
-            var item =_db.TblProducts.AsNoTracking().FirstOrDefault(x => x.ProductCode == productCode);
-            item.Name = productName;
-            item.Price = price;
-            item.ProductCategoryCode = productCategoryCode;
-            _db.Entry(item).State = EntityState.Modified;
-          var result = _db.SaveChanges();
-            return result;
-        }
+   public int ProductUpdate(string productCode, string productName, double price, string productCategoryCode )
+    {
+        var item =_db.TblProducts.AsNoTracking().FirstOrDefault(x => x.ProductCode == productCode);
+        item.Name = productName;
+        item.Price = price;
+        item.ProductCategoryCode = productCategoryCode;
+        _db.Entry(item).State = EntityState.Modified;
+      var result = _db.SaveChanges();
+        return result;
+    }
 
-        public int ProductDelete(string productCode)
-        {
-            var item = _db.TblProducts.AsNoTracking().FirstOrDefault(x => x.ProductCode == productCode);
-            item.DeleteFlag = true;
-            _db.Entry(item).State = EntityState.Modified;
-            var result = _db.SaveChanges();
-            return result;
-        }
+    public int ProductDelete(string productCode)
+    {
+        var item = _db.TblProducts.AsNoTracking().FirstOrDefault(x => x.ProductCode == productCode);
+        item.DeleteFlag = true;
+        _db.Entry(item).State = EntityState.Modified;
+        var result = _db.SaveChanges();
+        return result;
     }
 }
