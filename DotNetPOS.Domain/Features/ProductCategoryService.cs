a209b98c8460767232981cabea5247eaead1daa3
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-18 22:45:48 +0630

    -

--- DotNetPOS.Domain/Features/ProductCategoryService.cs
+++ DotNetPOS.Domain/Features/ProductCategoryService.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DotNetPOS.Database.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace DotNetPOS.Domain.Features
