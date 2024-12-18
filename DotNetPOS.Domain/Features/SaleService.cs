0237e0a4e3783da7dbc9d6e3bb34b269c6549556
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-18 22:48:28 +0630

    feat : get and Create Sale with Create SaleDetail

--- DotNetPOS.Domain/Features/SaleService.cs
+++ DotNetPOS.Domain/Features/SaleService.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNetPOS.Domain.Features;
 
-namespace DotNetPOS.Domain.Features
+public class SaleService
 {
-    public class SaleService
+    private readonly AppDbContext _db = new AppDbContext();
+
+    public List<TblSale> GetSales()
     {
+        var lst = _db.TblSales.ToList();
+        return lst;
+    }
+    
+    public int CreateSale(SaleRequestModel reqModel)
+    {
+        var voucherNo = Ulid.NewUlid().ToString();
+        _db.TblSaleDetails.Add(new TblSaleDetail
+        {
+            VoucherNo = voucherNo,
+            ProductCode = reqModel.ProductCode,
+            Quantity = reqModel.Quantity,
+            Price = reqModel.Price,
+        });
+
+        _db.TblSales.Add(new TblSale
+        {
+            VoucherNo = voucherNo,
+            SaleDate = DateTime.Now,
+            TotalAmount = reqModel.Quantity * reqModel.Price,
+        });
+        int result = _db.SaveChanges();
+        return result;
     }
 }
