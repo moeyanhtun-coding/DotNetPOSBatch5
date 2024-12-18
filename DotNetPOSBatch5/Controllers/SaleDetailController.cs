5e9d868c7134140f4f5c11d5690d40ebb5e087ad
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-18 22:47:25 +0630

    feat : GetSaleLists and GetSaleDetailByVoucherNo

---
+++ DotNetPOSBatch5/Controllers/SaleDetailController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetPOSBatch5.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SaleDetailController : Controller
+{
+    private readonly SaleDetailsService _saleDetailsService = new();
+    
+    [HttpGet]
+    public IActionResult GetSaleDetails()
+    {
+        var lst = _saleDetailsService.GetSaleDetails();
+        return Ok(lst);
+    }
+
+    [HttpGet("{voucherNo}")]
+    public IActionResult GetSaleDetailByVoucherNo(string voucherNo)
+    {
+        var saleDetail = _saleDetailsService.GetSaleDetailByVoucherNo(voucherNo);
+        return Ok(saleDetail);
+    }
+}
