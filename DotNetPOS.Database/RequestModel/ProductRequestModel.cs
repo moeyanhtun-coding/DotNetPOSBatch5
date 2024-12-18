11e9143a0e463bd00d866c1f2b92fa0c2e0488a9
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-18 15:20:27 +0630

    feat : add productRequestModel

---
+++ DotNetPOS.Database/RequestModel/ProductRequestModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DotNetPOS.Database.RequestModel;
+
+public class ProductRequestModel
+{
+    public string ProductName { get; set; }
+    public double Price { get; set; }
+    public string ProductCategoryCode { get; set; }
+}
