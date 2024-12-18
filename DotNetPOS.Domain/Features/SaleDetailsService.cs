c17d076c88d599772910188ea5cb8f2d98f10b42
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-18 22:47:47 +0630

    feat : SaleDetailService

--- DotNetPOS.Domain/Features/SaleDetailsService.cs
+++ DotNetPOS.Domain/Features/SaleDetailsService.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNetPOS.Domain.Features;
 
-namespace DotNetPOS.Domain.Features
+public class SaleDetailsService
 {
-    public class SaleDetailsService
+    private readonly AppDbContext _db = new AppDbContext();
+    public List<TblSaleDetail> GetSaleDetails()
     {
+        var lists = _db.TblSaleDetails.ToList()!;
+        return lists;
+    }
+
+    public TblSaleDetail? GetSaleDetailByVoucherNo(string voucherNo)
+    {
+        var saleDetail = _db.TblSaleDetails.FirstOrDefault(s => s.VoucherNo == voucherNo);
+        if (saleDetail is null)
+            return null;
+        return saleDetail;
     }
 }
