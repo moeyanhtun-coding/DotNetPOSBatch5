20950e108af3d11cba42d32f1e2417c22cdb83d1
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-18 22:48:00 +0630

    feat : sale request model

---
+++ DotNetPOS.Database/RequestModel/SaleRequestModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DotNetPOS.Database.RequestModel;
+
+public class SaleRequestModel
+{
+    public string ProductCode { get; set; }
+    public int Quantity { get; set; }
+    public double Price { get; set; }
+}
