1c25d2dd294b49fdc35bb20f6e5fdc6f78f172a5
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-18 22:45:12 +0630

    feat : add Global using

---
+++ DotNetPOS.Domain/GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿// Global using directives
+
+global using DotNetPOS.Database.Models;
+global using DotNetPOS.Database.RequestModel;
---
+++ DotNetPOSBatch5/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿// Global using directives
+
+global using DotNetPOS.Database.Models;
+global using DotNetPOS.Database.RequestModel;
+global using DotNetPOS.Domain.Features;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Http.HttpResults;
+global using Microsoft.AspNetCore.Mvc;
