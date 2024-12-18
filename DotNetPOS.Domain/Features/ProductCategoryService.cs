4e2d7aab4fdfea7a97ea60b43aa5f10f2612e11a
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-18 15:23:02 +0630

    -

--- DotNetPOS.Domain/Features/ProductCategoryService.cs
+++ DotNetPOS.Domain/Features/ProductCategoryService.cs
@@ -11,11 +11,13 @@ namespace DotNetPOS.Domain.Features
     public class ProductCategoryService
     {
         private readonly AppDbContext _db = new AppDbContext();
+
         public List<TblProductCategory> GetProductCategories()
         {
             var lst = _db.TblProductCategories.ToList();
             return lst;
         }
+
         public TblProductCategory GetProductCategoryByCode(string code)
         {
             var item = _db.TblProductCategories
@@ -25,6 +27,7 @@ namespace DotNetPOS.Domain.Features
                 return null!;
             return item;
         }
+
         public int PostProductCategory(string categoryName)
         {
             var item = new TblProductCategory
@@ -37,29 +40,30 @@ namespace DotNetPOS.Domain.Features
             return result;
         }
 
-        public int UpdateProductCategory(string categoryCode , string categoryName)
+        public int UpdateProductCategory(string categoryCode, string categoryName)
         {
             var item = _db.TblProductCategories
                 .AsNoTracking()
                 .FirstOrDefault(x => x.ProductCategoryCode == categoryCode);
-            if(item is null)
+            if (item is null)
                 return 0;
-            if(!string.IsNullOrEmpty(categoryName))
+            if (!string.IsNullOrEmpty(categoryName))
                 item.Name = categoryName;
             _db.Entry(item).State = EntityState.Modified;
             var result = _db.SaveChanges();
             return result;
         }
+
         public int DeleteProductCategory(string categoryCode)
         {
             var item = _db.TblProductCategories
                 .AsNoTracking()
-                .FirstOrDefault(x =>x.ProductCategoryCode == categoryCode);
+                .FirstOrDefault(x => x.ProductCategoryCode == categoryCode);
             if (item is null)
                 return 0;
             item.DeleteFlag = true;
             _db.Entry(item).State = EntityState.Modified;
-           var result = _db.SaveChanges();
+            var result = _db.SaveChanges();
             return result;
         }
     }
