a1593d5e56e1cc064f83e0f139176cd581dade94
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-18 22:46:38 +0630

    feat : create and get sale list

---
+++ DotNetPOSBatch5/Controllers/SaleController.cs
@@ -0,0 +1,25 @@
+﻿namespace DotNetPOSBatch5.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SaleController : Controller
+{
+    private readonly SaleService _saleService = new SaleService();
+    private readonly SaleDetailsService _saleDetailsService = new SaleDetailsService();
+
+    [HttpGet]
+    public IActionResult GetSalesList()
+    {
+        var list = _saleService.GetSales();
+        return Ok(list);
+    }
+
+    [HttpPost]
+    public IActionResult CreateSale(SaleRequestModel reqMole)
+    {
+        var result = _saleService.CreateSale(reqMole);
+        if (result is 0)
+            return BadRequest(new { message = "Sale not created" });
+        return Ok(new { message = "Sale created" });
+    }
+}
